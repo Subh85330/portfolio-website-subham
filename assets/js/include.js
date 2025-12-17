0b9de8508e71eb81f04553c5cb446bd8ed2c2cb4
Subham <subhamk356@gmail.com>
2025-12-17 11:58:00 +0530

    footer DRY Testing in Project page

--- assets/js/include.js
+++ assets/js/include.js
@@ -5,3 +5,11 @@ document.addEventListener("DOMContentLoaded", () => {
       document.getElementById("header-placeholder").innerHTML = html;
     });
 });
+
+document.addEventListener("DOMContentLoaded", () => {
+  fetch("/partials/footer.html")
+    .then(res => res.text())
+    .then(html => {
+      document.getElementById("footer-placeholder").innerHTML = html;
+    });
+});
