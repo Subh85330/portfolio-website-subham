839520468353f03eaf0fa0d430597554464994c4
Subham <subhamk356@gmail.com>
2025-12-18 13:54:16 +0530

    home background change

--- assets/js/app.js
+++ assets/js/app.js
@@ -23,13 +23,13 @@ particlesJS('particles-js',
         }
       },
       "color": {
-        "value": "#000000"
+        "value": "#f8f3f3ff"
       },
       "shape": {
         "type": "circle",
         "stroke": {
           "width": 0,
-          "color": "#000000"
+          "color": "#f1ececff"
         },
         "polygon": {
           "nb_sides": 5
@@ -63,7 +63,7 @@ particlesJS('particles-js',
       "line_linked": {
         "enable": true,
         "distance": 150,
-        "color": "#000000",
+        "color": "#2f94edff",
         "opacity": 0.4,
         "width": 1
       },
