92ae564806a13e48a98d4b637b85fcd9e280853f
Subham <subhamk356@gmail.com>
2025-12-18 14:10:51 +0530

    reverted home background color

--- assets/js/app.js
+++ assets/js/app.js
@@ -23,13 +23,13 @@ particlesJS('particles-js',
         }
       },
       "color": {
-        "value": "#040000ff"
+        "value": "#000000"
       },
       "shape": {
         "type": "circle",
         "stroke": {
           "width": 0,
-          "color": "#030000ff"
+          "color": "#000000"
         },
         "polygon": {
           "nb_sides": 5
@@ -63,7 +63,7 @@ particlesJS('particles-js',
       "line_linked": {
         "enable": true,
         "distance": 150,
-        "color": "#2f94edff",
+        "color": "#000000",
         "opacity": 0.4,
         "width": 1
       },
